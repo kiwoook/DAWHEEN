a4684f2067cb9a536789b35fa88f0f2a2e161860
kiwoook <timzfns1218@naver.com>
2024-01-11 15:27:06 +0900

    QureyDSL 설정

---
+++ dahween_backend/src/main/java/com/study/dahween/DahweenApplication.java
@@ -0,0 +1,13 @@
+package com.study.dahween;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DahweenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DahweenApplication.class, args);
+    }
+
+}
