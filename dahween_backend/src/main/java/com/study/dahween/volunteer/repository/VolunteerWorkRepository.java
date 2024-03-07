af9c9afcc640437274fdc9d42f88c6717fbb86a3
kiwoook <tizmfns1218@Naver.com>
2024-03-07 17:21:48 +0900

    Feat: 쿼리 최적화

--- dahween_backend/src/main/java/com/study/dahween/volunteer/repository/VolunteerWorkRepository.java
+++ dahween_backend/src/main/java/com/study/dahween/volunteer/repository/VolunteerWorkRepository.java
@@ -7,7 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface VolunteerWorkRepository extends JpaRepository<VolunteerWork, Long> {
+public interface VolunteerWorkRepository extends JpaRepository<VolunteerWork, Long>, VolunteerWorkRepositoryCustom {
 
     Optional<List<VolunteerWork>> getAllByOrganization(Organization organization);
+
 }
