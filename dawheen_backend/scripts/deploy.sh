9b001faab00f5b95007dca974569211f0234cc43
kiwoook <tizmfns1218@Naver.com>
2024-04-12 14:51:50 +0900

    Feat: 무중단 배포 구현

--- dawheen_backend/scripts/deploy.sh
+++ dawheen_backend/scripts/deploy.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-cd /home/ubuntu/action/dawheen_backend/
-
+sudo chmod 775 /home/ubuntu/action/deploy.log
 
 echo "> docker-compose 중지" >> /home/ubuntu/action/deploy.log
 
-# 이전에 실행 중인 컨테이너를 중지하고 삭제합니다.
-sudo docker-compose down
+cd /home/ubuntu/action/dawheen_backend/ || echo "> /home/ubuntu/action/dawheen_backend/ 디렉토리를 찾을 수 없습니다." >> /home/ubuntu/action/deploy.log
 
-# 이전에 실행 중인 컨테이너의 네트워크 및 볼륨을 정리합니다.
-sudo docker network prune -f
+EXIST_BLUE=$(sudo docker-compose -p dawheen_backend-blue -f docker-compose.blue.yml ps | grep Up)
 
-echo "> docker-compose 작동" >> /home/ubuntu/action/deploy.log
+if [ -z "$EXIST_BLUE" ]; then
+    echo "blue up"
+    sudo docker-compose -p dawheen_backend-blue -f docker-compose.blue.yml up -d
+    BEFORE_COMPOSE_COLOR="green"
+    AFTER_COMPOSE_COLOR="blue"
+else
+    echo "green up"
+    sudo docker-compose -p dawheen_backend-green -f docker-compose.green.yml up -d
+    BEFORE_COMPOSE_COLOR="blue"
+    AFTER_COMPOSE_COLOR="green"
+fi
 
+sleep 10
 
-sudo docker-compose up -d --remove-orphans
+EXIST_AFTER=$(sudo docker-compose -p dawheen_backend-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
 
+if [ -n "$EXIST_AFTER" ]; then
+   # nginx.config 를 컨테이너에 맞게 변경해주고 reload 한다
+    sudo cp /etc/nginx/dawheen_backend.${AFTER_COMPOSE_COLOR}.conf /etc/nginx/nginx.conf
+    sudo nginx -s reload
 
+    # 이전 컨테이너 종료
+    sudo docker-compose -p dawheen_backend-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
+    echo "$BEFORE_COMPOSE_COLOR down"
+fi
+
+echo "> docker-compose 작동 완료" >> /home/ubuntu/action/deploy.log
